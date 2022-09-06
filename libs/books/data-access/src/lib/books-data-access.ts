5c32484d6e2b014beecc60a2be55f728dadd8ff2
Itunu Lamina <nix4phun@gmail.com>
2022-09-06 17:58:11 +0100

    added api app

--- libs/books/data-access/src/lib/books-data-access.ts
+++ libs/books/data-access/src/lib/books-data-access.ts
@@ -7,43 +7,10 @@ interface BookType {
 }
 
 export const getBooks = async (): Promise<BookType[]> => {
-  // TODO: We'll wire this up to an actual API later.
-  // For now we are just returning some fixtures.
-  return [
-    {
-      id: 1,
-      title: 'The Picture of Dorian Gray',
-      author: 'Oscar Wilde',
-      rating: 3,
-      price: 9.99,
+  const data = await fetch('/api/books', {
+    headers: {
+      'Content-Type': 'application/json',
     },
-    {
-      id: 2,
-      title: 'Frankenstein',
-      author: 'Mary Wollstonecraft Shelley',
-      rating: 5,
-      price: 7.95,
-    },
-    {
-      id: 3,
-      title: 'Jane Eyre',
-      author: 'Charlotte Brontë',
-      rating: 4,
-      price: 10.95,
-    },
-    {
-      id: 4,
-      title: 'Dracula',
-      author: 'Bram Stoker',
-      rating: 5,
-      price: 14.99,
-    },
-    {
-      id: 5,
-      title: 'Pride and Prejudice',
-      rating: 4,
-      author: 'Jane Austen',
-      price: 12.85,
-    },
-  ];
+  });
+  return data.json();
 };
